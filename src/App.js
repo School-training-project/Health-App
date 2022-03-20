bd74f58925388d9f7e345eae87109e95c7fcecbf
Arij-Chafter <chafter.arij@gmail.com>
2022-03-20 19:21:18 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import test from './pages/index.js';
-import map from './pages/map'
-import Progress from './pages/progress.js';
-import quiz from './pages/quiz';
-import { signout } from './pages/signout';
-import { blog } from './pages/blog';
+import test from "./pages/index.js";
+import map from "./pages/map";
+import Progress from "./pages/progress.js";
+import quiz from "./pages/quiz";
+import { signout } from "./pages/signout";
+import { blog } from "./pages/blog";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Switch>
-        <Route exact path="/" component={test}/>
-        <Route path="/progress" component={Progress} />
-        <Route path="/quiz" component={quiz} />
-        <Route path="/blog" component={blog} />
-        <Route path="/map" exact component={map}/>
-        <Route path="/signin" component={signout} />
+        
+         <Route path="/signin" component={signout} />
+         <div>
+         <Navbar />
+            <Route exact path="/" component={test} />
+            <Route path="/progress" component={Progress} />
+            <Route path="/quiz" component={quiz} />
+            <Route path="/blog" component={blog} />
+            <Route path="/map" exact component={map} />
+        </div>
       </Switch>
     </Router>
   );
-};
+}
 
 export default App;
