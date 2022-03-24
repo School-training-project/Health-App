1aa88a8a830b2de32c3a44dd0568089f8f245811
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2022-03-24 15:50:27 +0100

    progress page

---
+++ src/components/Item/Item.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.div `
+    display:flex;
+    height:500px;
+    width: 100%;
+    background-color:#FFFFFF00;
+    color: #FFF;
+    margin 0 20px;
+    font-size: 3rem;
+`
--- src/pages/progress.js
+++ src/pages/progress.js
@@ -1,16 +1,55 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { BarChart } from "../components/Charts/Barchart";
+import { UserData } from "../components/Charts/Data";
+import { LineChart } from "../components/Charts/LineChart";
+import { PieChart } from "../components/Charts/PieChart";
+import Carousel from "react-elastic-carousel"
+import Item from "../components/Item/Item";
 const Progress = () => {
+    const [userData, setUserData] = useState({
+        labels: UserData.map((data)=> data.year),
+        datasets: [{
+            label: "Users Gained",
+            data: UserData.map((data)=> data.userGain),
+            backgroundColor: ["#82C272", "#00A88F", "#0087AC", "#005FAA", "#323B81"],
+            borderColor: "gray",
+            borderWidth:"2"
+        }]
+    })
     return (
-        <div
-            style={{
+        <div  style={{
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
                 height: '100vh'
-            }}
-        >
-            <h1>Progress</h1>
+            }}>  
+            <Carousel style={{color:'red'}} >
+                <Item>
+                    <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',}}>
+                        <LineChart DataPassed={userData}/>
+                    </div>
+                </Item>
+                <Item>
+                    <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                }}> 
+                            <BarChart DataPassed={userData}/>
+                    </div>
+                </Item>
+                <Item>
+                    <div>
+                            <PieChart DataPassed={userData}/>
+                    </div>
+                </Item>
+                <Item>
+                    Form for data input hatta se3a yhelha raby
+                </Item>
+            </Carousel>
         </div>
     );
 };
