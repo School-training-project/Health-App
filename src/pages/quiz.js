90f7f541389fb5577f8765bd0a700194838eacb3
MJ <mouhebjemai123@gmail.com>
2022-03-27 15:22:06 +0100

    Added quiz page

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import Q from '../components/Quiz/Quizpage';
+//import mra from '../components/Quiz/mra.png';
 
 function quiz() {
     return (
         <div
             style={{
                 display: 'flex',
+                flexDirection:'column',
                 justifyContent: 'center',
                 alignItems: 'center',
                 height: '100vh'
             }}
         >
-           <h1>Quiz</h1> 
+       
+           <Q />
         </div>
     );
 };
