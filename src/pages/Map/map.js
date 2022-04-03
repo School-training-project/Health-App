e357798543158a260eeff83ec415e1a637f8e09b
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2022-04-03 14:28:51 +0100

    Darkmode improvements+map

--- src/pages/Map/map.js
+++ src/pages/Map/map.js
@@ -1,17 +1,46 @@
-import React from "react";
-import { LineLayer } from "@deck.gl/layers";
-import renderToDOM from "../../components/DeckGl/DeckGL";
-import DeckGL1 from "../../components/DeckGl/DeckGL";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, { useState, useRef, useCallback } from "react";
+import { render } from "react-dom";
+import MapGL from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
 
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoic3VwZXJoMjU3IiwiYSI6ImNsMWM4ajdkOTA1OGQzam5yampkM3c4aDEifQ.TO-_alEhY5rydxgDGaF_Kw';
+const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoic3VwZXJoMjU3IiwiYSI6ImNsMWM4ajdkOTA1OGQzam5yampkM3c4aDEifQ.TO-_alEhY5rydxgDGaF_Kw";
 
-function Map () {
+function Map1() {
+  const [viewport, setViewport] = useState({
+    longitude: 10.1815,
+    latitude: 36.8065,
+    zoom: 8
+  });
+  const geocoderContainerRef = useRef();
+  const mapRef = useRef();
+  const [mapstyle,setmap]=useState("mapbox://styles/superh257/cl1ia13ee008715qs6lflsbmm")
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides
+      });
+    },
+    [handleViewportChange]
+  );
+
   return (
     <div>
       <div
         style={{
           display: "flex",
-          backgroundColor: "#cf6679f1",
+          backgroundColor: "var(--back-div)",
           color: "currentColor",
           borderRadius: "80px",
           justifyContent: "center",
@@ -28,41 +57,70 @@ function Map () {
               transform: "translate(0,20px)",
             }}
           >
-            Nutrition Stores
+            <a href="#" style={{color: 'var(--text-color)',transition: 'background .3s ease-in-out, color .6s ease-in-out',textDecoration: 'inherit',cursor: 'inherit'}}>Nutrition Stores</a>
           </li>
           <li
             style={{
               transform: "translate(0,200px)",
             }}
           >
-            Pinned locations
+            <a href="#" style={{color: 'var(--text-color)',transition: 'background .3s ease-in-out, color .6s ease-in-out',textDecoration: 'inherit',cursor: 'inherit'}}>Pinned Locations</a>
           </li>
         </ul>
-        <iframe src="https://giphy.com/embed/3gJTIZdFKjngOEhSSG" width="300" height="300" frameBorder="0" class="giphy-embed" style={{objectFit: "cover",transform: "translate(0px,-45px)"}} ></iframe>
+        <iframe
+          src="https://giphy.com/embed/3gJTIZdFKjngOEhSSG"
+          width="300"
+          height="300"
+          frameBorder="0"
+          class="giphy-embed"
+          style={{ objectFit: "cover", transform: "translate(0px,-45px)" }}
+        ></iframe>
         <ul>
           <li
             style={{
               transform: "translate(0,20px)",
             }}
           >
-            Infirmary
+            <a href="#" style={{color: 'var(--text-color)',transition: 'background .3s ease-in-out, color .6s ease-in-out',textDecoration: 'inherit',cursor: 'inherit'}}>Infirmary</a>
           </li>
           <li
             style={{
               transform: "translate(0,200px)",
             }}
           >
-            Pharmacies
+            <a href="#" style={{color: 'var(--text-color)',transition: 'background .3s ease-in-out, color .6s ease-in-out',textDecoration: 'inherit',cursor: 'inherit'}}>Pharmacies</a>
           </li>
         </ul>
-      </div> 
-      <div id="map"> 
-        <DeckGL1>
-        </DeckGL1>
+      </div>
+     <div className="map-container">
+      <div
+        ref={geocoderContainerRef}
+        style={{ position: "absolute", top: 20, left: 20, zIndex: 1 ,borderTopRightRadius:'50px',
+        borderTopLeftRadius:'50px'}}
+      />
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        mapStyle="mapbox://styles/superh257/cl1ia13ee008715qs6lflsbmm"
+        width="100%"
+        height="100%"
+        style={{borderTopRightRadius:'50px',
+          borderTopLeftRadius:'50px'}}
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Geocoder
+          mapRef={mapRef}
+          containerRef={geocoderContainerRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+          
+        />
+      </MapGL>
     </div>
     </div>
   );
 }
 
-
-export default Map;
+export default Map1;
