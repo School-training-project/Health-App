d873c90c36024af2391d2d82628460b9adce7906
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2022-04-10 16:00:01 +0100

    basic crud

---
+++ src/db/services/services.js
@@ -0,0 +1,62 @@
+const db= require("../dbConnection")
+
+
+
+const createUser= async (obj)=> {
+    const credential= new db.Credential({
+        name:obj.name,
+        email:obj.email,
+        password:obj.password})
+    await credential.save()
+}
+
+
+const createUserData=async(obj)=>{
+    const data= new db.Data({
+        email:obj.email,
+        caloriesBurnt:obj.caloriesBurnt,
+        stepsWalked:obj.stepsWalked,
+        caloriesIncome:obj.caloriesIncome,
+        hoursSlept:obj.hoursSlept,
+    })
+    await data.save()
+}
+
+
+const getUserData= async (obj={})=>{
+    const data= await db.Data.find(obj)
+    console.log(data)
+}
+
+const getUser = async (obj={}) => {
+    const users = await db.Credential.find(obj);
+    console.log(users);
+};
+
+const updateUser = async (obj={},cond)=>{
+    const result = await db.Credential.updateOne({filter:cond},{$set:obj})
+    console.log(result)
+} 
+
+const updateUserData = async (obj={},cond)=>{
+    const result = await db.Data.updateOne({filter:cond},{$set:obj})
+    console.log(result)
+} 
+
+const removeUser = async (id) =>{
+    const result = await db.Credential.deleteOne({_id:id})
+    console.log(result)
+}
+const removeUserData = async (obj) =>{
+    const result = await db.Data.deleteOne(obj)
+    console.log(result)
+}
+
+module.exports.removeUser=removeUser
+module.exports.removeUserData=removeUserData
+module.exports.updateUser=updateUser
+module.exports.updateUserData=updateUserData
+module.exports.createUser=createUser
+module.exports.getUser=getUser
+module.exports.getUserData=getUserData
+module.exports.createUserData=createUserData
