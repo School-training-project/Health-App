d0b118f44a24a9234bd3a5d49e0fff3a513e48b3
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2022-05-09 11:00:31 +0100

    final changes ...

--- src/components/Blog/HealthArticle.js
+++ src/components/Blog/HealthArticle.js
@@ -10,7 +10,7 @@ function HealthArticle({ data }) {
       <img className="news__img" src={data.urlToImage} alt ="Image currently unavailable"></img> <br />
       <span className="news__source">Source : {data.source.name}</span><br />
       <span className="news__published">Published : {data.publishedAt}</span><br />
-      <a  href={data.url} className="news__button">
+      <a  href={data.url} target="_blank" className="news__button">
           View More
       </a>
     </div>
